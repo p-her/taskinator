2d9edc44a4fa6d519471588744551f9bf9eb6f47
Pao Her <p.her007@hotmail.com>
2021-12-17 12:31:54 -0600

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+   
+
+    event.preventDefault();
+
+    
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -11,4 +17,4 @@ var createTaskHandler = function () {
     console.log(tasksToDoEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
